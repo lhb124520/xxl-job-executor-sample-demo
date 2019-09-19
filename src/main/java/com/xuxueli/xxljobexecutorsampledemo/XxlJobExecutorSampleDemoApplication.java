f9a323397913debb02505c05722a5c1416acb214
刘海波 <1282072571@qq.com>
2019-09-19 17:08:08 +0800

    新增

---
+++ src/main/java/com/xuxueli/xxljobexecutorsampledemo/XxlJobExecutorSampleDemoApplication.java
@@ -0,0 +1,13 @@
+package com.xuxueli.xxljobexecutorsampledemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XxlJobExecutorSampleDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XxlJobExecutorSampleDemoApplication.class, args);
+    }
+
+}
